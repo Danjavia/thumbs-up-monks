a9bbb89037a4f6ade06376ebeb4d0e356491a5e5
danjavia <danjavia@gmail.com>
2024-06-11 06:04:47 -0500

    #Grid: Perform grid styles

--- components/Item/Grid.vue
+++ components/Item/Grid.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { ref, computed } from "vue";
-import { defineProps } from "vue";
 import { formatDistanceToNow } from "date-fns";
 
 const props = defineProps<{
+  id: string;
   name: string;
   description: string;
   category: string;
@@ -14,6 +14,8 @@ const props = defineProps<{
 
 const positiveVotes = ref(props.votes.positive);
 const negativeVotes = ref(props.votes.negative);
+const voteSelected = ref<"positive" | "negative" | null>(null);
+const voteSubmitted = ref(false);
 
 const totalVotes = computed(() => positiveVotes.value + negativeVotes.value);
 const positivePercentage = computed(() =>
@@ -23,41 +25,109 @@ const negativePercentage = computed(() =>
   ((negativeVotes.value / totalVotes.value) * 100).toFixed(1),
 );
 
-const vote = (type: "positive" | "negative") => {
-  if (type === "positive") positiveVotes.value++;
-  else negativeVotes.value++;
+const emit = defineEmits<{
+  (e: "vote", payload: { id: string; type: "positive" | "negative" }): void;
+}>();
+
+const voteNow = () => {
+  if (voteSelected.value) {
+    emit("vote", { id: props.id, type: voteSelected.value });
+    voteSelected.value = null;
+  }
+};
+
+const selectVote = (type: "positive" | "negative") => {
+  voteSelected.value = voteSelected.value === type ? null : type;
 };
 
 const timeAgo = computed(() =>
-  formatDistanceToNow(new Date(props.lastUpdated), { addSuffix: true }),
+  formatDistanceToNow(new Date(props.lastUpdated), {
+    addSuffix: true,
+  }),
 );
 </script>
 
 <template>
-  <div class="grid-item">
+  <div class="grid-item relative">
     <NuxtImg
-      :src="`/img/participants/${picture}`"
+      :src="`/img/participants/${props.picture}`"
       alt="Profile Image"
       class="grid-item__image"
       sizes="100vw sm:50vw md:400px"
       role="none"
     />
+
     <div class="grid-item__content">
-      <h2>{{ name }}</h2>
-      <p>{{ description }}</p>
-      <span>{{ timeAgo }} in {{ category }}</span>
-      <div class="grid-item__actions">
-        <button @click="vote('positive')" class="btn like-btn">👍</button>
-        <button @click="vote('negative')" class="btn dislike-btn">👎</button>
-        <button class="btn vote-btn">Vote Now</button>
+      <div class="flex">
+        <div class="flex items-end">
+          <div
+            v-if="positivePercentage > negativePercentage"
+            class="btn like-status relative -top-1 hover:border-0"
+          >
+            <img src="assets/img/thumbs-up.svg" alt="thumbs up" />
+          </div>
+          <div v-else class="btn dislike-status relative -top-1 hover:border-0">
+            <img src="assets/img/thumbs-down.svg" alt="thumbs down" />
+          </div>
+          <h2
+            class="text-white text-3xl font-[400] mb-1 ml-2 text-left line-clamp-2 w-[85%]"
+          >
+            {{ props.name }}
+          </h2>
+        </div>
+      </div>
+
+      <div class="w-2/3 mx-auto">
+        <p class="font-thin text-xs line-clamp-2">
+          {{ props.description }}
+        </p>
+      </div>
+
+      <div class="mt-3 w-[90%]">
+        <div class="text-right mb-3 text-xs font-semibold">
+          {{ voteSubmitted ? "Thank you for your vote!" : timeAgo }} in
+          {{ category }}
+        </div>
+
+        <div class="grid-item__actions">
+          <button
+            @click="selectVote('positive')"
+            :class="[
+              'btn',
+              'like-btn',
+              { selected: voteSelected === 'positive' },
+            ]"
+          >
+            <img src="assets/img/thumbs-up.svg" alt="thumbs up" />
+          </button>
+          <button
+            @click="selectVote('negative')"
+            :class="[
+              'btn',
+              'dislike-btn',
+              { selected: voteSelected === 'negative' },
+            ]"
+          >
+            <img src="assets/img/thumbs-down.svg" alt="thumbs down" />
+          </button>
+          <button
+            @click="voteNow"
+            :disabled="!voteSelected"
+            class="btn vote-btn"
+          >
+            Vote Now
+          </button>
+        </div>
       </div>
     </div>
     <div class="grid-item__votes">
       <div class="like-bar" :style="{ width: positivePercentage + '%' }">
-        👍 {{ positivePercentage }}%
+        <img src="assets/img/thumbs-up.svg" alt="thumbs up" class="mr-2" />
+        {{ positivePercentage }}%
       </div>
       <div class="dislike-bar" :style="{ width: negativePercentage + '%' }">
-        👎 {{ negativePercentage }}%
+        <img src="assets/img/thumbs-down.svg" alt="thumbs down" class="mr-2" />
+        {{ negativePercentage }}%
       </div>
     </div>
   </div>
@@ -65,40 +135,127 @@ const timeAgo = computed(() =>
 
 <style scoped>
 .grid-item {
+  position: relative;
   border: 1px solid #ccc;
-  padding: 1rem;
-  margin-bottom: 1rem;
+  color: white;
   text-align: center;
+  margin-bottom: 1rem;
 }
 
 .grid-item__image {
   width: 100%;
-  height: auto;
+  height: 100%;
 }
 
 .grid-item__content {
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0.0001) 0%,
+    rgba(0, 0, 0, 0.6) 100%
+  );
+  position: absolute;
+  bottom: 30px;
+  left: 0;
   padding: 1rem 0;
 }
 
+.grid-item__actions {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+
 .grid-item__actions .btn {
-  margin: 0.5rem;
+  margin: 0 0.5rem;
 }
 
 .grid-item__votes {
+  position: absolute;
+  bottom: 0;
+  left: 0;
   display: flex;
   width: 100%;
 }
 
 .like-bar,
 .dislike-bar {
-  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  color: white;
+  font-weight: bold;
 }
 
 .like-bar {
-  background-color: #4caf50;
+  background-color: rgba(60, 187, 180, 0.6);
+  width: 50%;
 }
 
 .dislike-bar {
-  background-color: #f44336;
+  background-color: rgba(249, 173, 29, 0.6);
+  width: 50%;
+}
+
+.like-btn,
+.dislike-btn,
+.like-status,
+.dislike-status {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 45px;
+  width: 45px;
+}
+
+.like-btn:hover,
+.like-btn:active,
+.dislike-btn:hover,
+.dislike-btn:active {
+  border: 2px solid white;
+}
+
+.like-btn,
+.like-status {
+  background-color: rgba(60, 187, 180, 1);
+  border: 1px solid rgba(60, 187, 180, 1);
+}
+
+.dislike-btn,
+.dislike-status {
+  background-color: rgba(249, 173, 29, 1);
+  border: 1px solid rgba(249, 173, 29, 1);
+}
+
+.btn.vote-btn {
+  background-color: #333;
+  color: white;
+  border: 2px solid white;
+  padding: 0.5rem 1rem;
+  margin-left: 0.3rem;
+  margin-right: 0;
+  transition: background-color 0.3s;
+}
+
+.vote-btn:disabled {
+  background-color: rgba(0, 0, 0, 0.6);
+  border-color: #fff;
+  cursor: not-allowed;
+}
+
+.vote-btn:hover:not(:disabled) {
+  background-color: #555;
+}
+
+.btn.selected {
+  border: 2px solid white;
+}
+
+.line-clamp-2 {
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 </style>
