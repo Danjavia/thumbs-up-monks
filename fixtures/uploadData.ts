1e948b8b2afe0c1cf935f96c4f982f90eaa53b98
danjavia <danjavia@gmail.com>
2024-06-11 03:43:10 -0500

    #DB: Populate data on first time

--- fixtures/uploadData.ts
+++ fixtures/uploadData.ts
@@ -10,21 +10,23 @@ export const checkAndCreateCollection = async () => {
   // Check if the collection exists by trying to get documents from it
   const querySnapshot = await getDocs(itemsRef);
 
-  console.log("empty ==>", querySnapshot.empty);
-
   // If collection is empty, populate with seed data
-  try {
-    for (const item of data) {
-      console.log("Item to aggregate ==>", item);
-      try {
-        // await itemsCollection;
-        await addDoc(itemsRef, item);
-        console.log(`Item ${item.name} added successfully`);
-      } catch (e) {
-        console.error(`Error adding item ${item.name}: `, e);
+  if (querySnapshot.empty) {
+    try {
+      for (const item of data) {
+        console.log("Item to aggregate ==>", item);
+        try {
+          // await itemsCollection;
+          await addDoc(itemsRef, item);
+          console.log(`Item ${item.name} added successfully`);
+        } catch (e) {
+          console.error(`Error adding item ${item.name}: `, e);
+        }
       }
+    } catch (error) {
+      console.error(`Error adding collection: ${error.message}`);
     }
-  } catch (error) {
-    console.error(`Error adding collection: ${error.message}`);
+  } else {
+    console.log("Database has been filled!");
   }
 };
